5cd92d53be024e09695981228f609070f14acc27
Konstantin <kosovmaster@yandex.ru>
2024-07-13 20:51:36 +0300

    feat: add feature_comments

--- ewm-service/src/main/java/ru/practicum/comment/controller/PrivateCommentController.java
+++ ewm-service/src/main/java/ru/practicum/comment/controller/PrivateCommentController.java
@@ -24,14 +24,14 @@ public class PrivateCommentController {
     @PostMapping("/event/{eventId}")
     @ResponseStatus(HttpStatus.CREATED)
     public CommentDto createComment(@PathVariable @NotNull @Positive Long eventId,
-                                    @PathVariable @NotNull @Positive Long userId,
+                                    @RequestParam @NotNull @Positive Long userId,
                                     @Valid @RequestBody NewCommentDto newCommentDto) {
         return commentService.createComment(eventId, userId, newCommentDto);
     }
 
     @PatchMapping("/{commentId}")
     public CommentDto updateComment(@PathVariable @NotNull @Positive Long commentId,
-                                    @PathVariable @NotNull @Positive Long userId,
+                                    @RequestParam @NotNull @Positive Long userId,
                                     @Valid @RequestBody UpdatedCommentDto updatedCommentDto) {
         return commentService.updateComment(commentId, userId, updatedCommentDto);
     }
@@ -39,13 +39,13 @@ public class PrivateCommentController {
     @DeleteMapping("/{commentId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteComment(@PathVariable @NotNull @Positive Long commentId,
-                              @PathVariable @NotNull @Positive Long userId) {
+                              @RequestParam @NotNull @Positive Long userId) {
         commentService.deleteComment(commentId, userId);
     }
 
     @GetMapping("/{commentId}")
     public CommentDto getComment(@PathVariable @NotNull @Positive Long commentId,
-                                 @PathVariable @NotNull @Positive Long userId) {
+                                 @RequestParam @NotNull @Positive Long userId) {
         return commentService.getCommentUser(commentId, userId);
     }
 
